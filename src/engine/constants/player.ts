d944fe0bb877ead5792e2aa0a5548c03e19ac742
Himani Shah <himanishah202@gmail.com>
2026-08-01 18:30:34 +0530

    Add player movement tuning constants

---
+++ src/engine/constants/player.ts
@@ -0,0 +1,8 @@
+export const EYE_HEIGHT = 1.6;
+export const BASE_SPEED = 4;
+export const SPRINT_MULTIPLIER = 1.8;
+export const MOVEMENT_RESPONSIVENESS = 8;
+export const HEAD_BOB_AMPLITUDE = 0.05;
+export const HEAD_BOB_FREQUENCY = 10;
+export const JUMP_VELOCITY = 5;
+export const GRAVITY = 18;
